a4a802efc7cd7bbb4fa25b39d0564a5ccd6292af
Kodam Sai Charan <96488659+Saicharan2k2@users.noreply.github.com>
2025-05-10 18:50:52 +0530

    Update StockController.java

--- Stock-API-Backend/Stock-Price-Service/src/main/java/com/example/demo/controller/StockController.java
+++ Stock-API-Backend/Stock-Price-Service/src/main/java/com/example/demo/controller/StockController.java
@@ -1,21 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.model.StockResponse;
 import com.example.demo.service.StockService;
 
 @RestController
-@RequestMapping("/api/stocks")
 @CrossOrigin(origins = "*")
 public class StockController {
 
     @Autowired
     private StockService stockService;
 
-    @GetMapping(value = "/{symbol}", produces = "application/json")
+    @GetMapping(value = "/api/stocks/{symbol}", produces = "application/json")
     public ResponseEntity<StockResponse> getStock(@PathVariable String symbol) throws Exception {
         StockResponse stockData = stockService.getStockData(symbol);
         return ResponseEntity.ok(stockData);
